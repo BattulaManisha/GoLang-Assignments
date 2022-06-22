6b5bc3542518a27bc1276c70e3bad46bf4a30cb6
BattulaManisha <99684612+BattulaManisha@users.noreply.github.com>
2022-06-22 21:35:51 +0530

    Delete SpringRest Assignment directory

--- SpringRest Assignment/SpringRest 9th/src/main/java/com/SriRam/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SriRam.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.SriRam.model.Customer;
-
-public interface CustomerRepository extends MongoRepository<Customer,Integer>{
-
-}
